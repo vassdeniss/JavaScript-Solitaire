97bdf8623ca50f874d7abb6997fab653ef6bc25a
Denis Vasilev <vassdeniss@gmail.com>
2023-02-15 21:13:26 +0200

    Implemented deck deal function

--- src/app.js
+++ src/app.js
@@ -1,25 +1,14 @@
-import { faces, suits, Stock, Waste, Foundation, Pile } from './cards.js';
-import { createDeck, shuffleDeck } from './utils.js';
+import { createDeck, shuffleDeck, dealDeck } from './utils.js';
 
 start();
 
 function start() {
-  const state = {
-    stock: new Stock(),
-    waste: new Waste(),
-    foundations: {
-      [suits.Clubs]: new Foundation([], suits.Clubs),
-      [suits.Diamonds]: new Foundation([], suits.Diamonds),
-      [suits.Hearts]: new Foundation([], suits.Hearts),
-      [suits.Spades]: new Foundation([], suits.Spades),
-    },
-    piles: new Array(7).fill(new Pile()),
-  };
-
   const deck = createDeck();
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     shuffleDeck(deck);
   }
 
-  console.log(deck);
+  const state = dealDeck(deck);
+
+  console.log(state);
 }
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,13 @@
-import { faces, suits, Card, Deck } from './cards.js';
+import {
+  faces,
+  suits,
+  Card,
+  Deck,
+  Stock,
+  Waste,
+  Foundation,
+  Pile,
+} from './cards.js';
 
 export function createDeck() {
   const deck = new Deck();
@@ -21,4 +30,42 @@ export function shuffleDeck(deck) {
   }
 
   deck.cards.push(...stock);
+}
+
+export function dealDeck(deck) {
+  const state = {
+    stock: new Stock(),
+    waste: new Waste(),
+    foundations: {
+      [suits.Clubs]: new Foundation([], suits.Clubs),
+      [suits.Diamonds]: new Foundation([], suits.Diamonds),
+      [suits.Hearts]: new Foundation([], suits.Hearts),
+      [suits.Spades]: new Foundation([], suits.Spades),
+    },
+    piles: [
+      new Pile(),
+      new Pile(),
+      new Pile(),
+      new Pile(),
+      new Pile(),
+      new Pile(),
+      new Pile(),
+    ],
+  };
+
+  for (let i = 0; i < 7; i++) {
+    debugger;
+
+    const pile = state.piles[i];
+
+    for (let j = 0; j <= i; j++) {
+      pile.cards.push(deck.cards.pop());
+    }
+
+    pile.top.faceUp = true;
+  }
+
+  state.stock.cards.push(...deck.cards);
+
+  return state;
 }
